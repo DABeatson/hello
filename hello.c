eaf184a6d75df2d87313437bdeaec370ca01871c
joshud47 <98373262+joshud47@users.noreply.github.com>
2022-08-28 14:11:22 -0500

    Update hello.c
    added my name and major

--- hello.c
+++ hello.c
@@ -9,6 +9,9 @@ int main(void) {
 
     printf("My name is Damien Beatson!\n");
     printf("My major is computer engineering\n");
+    
+    printf("My name is Joshua Davis!\n");
+    printf("My major is Computer Engineering\n");
     return 0;
 
 }
